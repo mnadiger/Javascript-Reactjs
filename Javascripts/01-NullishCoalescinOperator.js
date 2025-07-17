6840734f3aa03e00d1e9c6de780e5a94c59891cf
Mariyanna Nadiger <mariyanna123@gmail.com>
2025-07-17 11:04:05 +0530

    Nullish Coalescin Operator(??):
     Nullish Coalescin Operator(??):

---
+++ Javascripts/01-NullishCoalescinOperator.js
@@ -0,0 +1,35 @@
+// Nullish Coalescin Operator(??):
+
+  - The nullish coalescing operator (??) in JavaScript provides a concise way to handle cases where a variable might be null or undefined.
+  - It returns the right-hand side operand if the left-hand side is null or undefined; otherwise, it returns the left-hand side value. 
+
+// How it Works:
+
+  -  The operator is represented by two question marks: ??. 
+  -  It evaluates the left-hand side expression. 
+  -  If the left-hand side is null or undefined, it returns the right-hand side expression. 
+  -  If the left-hand side is not null or undefined (even if it's a falsy value like 0 or an empty string), it returns the left-hand side value. 
+
+
+// Example:
+
+    let user = null;
+    let name = user ?? "Guest";
+    console.log(name); // Output: Guest
+    
+    let count = 0;
+    let displayCount = count ?? 10;
+    console.log(displayCount); // Output: 0
+    
+    let message = "";
+    let defaultMessage = message ?? "No message";
+    console.log(defaultMessage); // Output: ""
+
+
+
+
+// Comparison with logical OR (||):
+
+  -  The nullish coalescing operator is similar to the logical OR (||) operator, but with a key difference. 
+  -  The logical OR operator returns the right-hand side operand if the left-hand side is a "falsy" value (e.g., null, undefined, 0, "", false). 
+  -  The nullish coalescing operator only returns the right-hand side if the left-hand side is specifically null or undefined.
